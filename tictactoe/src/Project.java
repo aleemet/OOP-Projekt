c403ef61ad3b638ab298ac8ab31f275063f04aca
martinvilt <martin_vilt@hotmail.com>
2016-03-12 16:16:11 +0200

    Added files via upload

---
+++ tictactoe/src/Project.java
@@ -0,0 +1,23 @@
+
+
+public class Project {
+    public static int[][] field = new int[3][3];{
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = 0;
+            }
+
+        }
+    }
+    public static void checkGameOver() {
+
+    }
+
+
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
